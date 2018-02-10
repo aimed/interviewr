c360a00b12c82adf4b0bd9a1452a164f2233058a
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-02-10 11:36:58 +0300

    feat(SkillGroupType): adds skills to SkillGroupType

--- packages/server/src/graphql/types/SkillGroupType.ts
+++ packages/server/src/graphql/types/SkillGroupType.ts
@@ -1,8 +1,9 @@
-import { GraphQLNonNull, GraphQLObjectType, GraphQLObjectTypeConfig, GraphQLString } from 'graphql';
+import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLObjectTypeConfig, GraphQLString } from 'graphql';
 import { graphQlIDField, graphlQLObjectName } from "../utils";
 
 import { InterviewrResolverContext } from '../context';
 import { SkillGroup } from '../../entities/SkillGroup';
+import { SkillType } from './SkillType';
 import { UserType } from './UserType';
 import { nodeInterface } from '../nodeDefinitions';
 
@@ -16,6 +17,12 @@ export const SkillGroupType = new GraphQLObjectType({
             type: new GraphQLNonNull(UserType),
             resolve(source, args, context) {
                 return source.user;
+            }
+        },
+        skills: {
+            type: new GraphQLNonNull(new GraphQLList(SkillType)),
+            resolve(source, args, context) {
+                return source.skills;
             }
         }
     })
