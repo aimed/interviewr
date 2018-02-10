44773ef38dda17c15fe8cb0d090e2fe7722abcef
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-02-10 11:36:36 +0300

    feat: adds a safeIdFetcher to help resolve globalIds

--- packages/server/src/graphql/nodeDefinitions.ts
+++ packages/server/src/graphql/nodeDefinitions.ts
@@ -1,11 +1,27 @@
 import { fromGlobalId, nodeDefinitions } from "graphql-relay";
 
+import { GraphQLResolveInfo } from "graphql";
 import { InterviewrResolverContext } from "./context";
 
-export const { nodeInterface, nodeField } = nodeDefinitions<InterviewrResolverContext>((globalId, context, info) => {
+export const idFetcher = <T>(globalId: string, context: InterviewrResolverContext, info?: GraphQLResolveInfo): Promise<T | null> => {
     const { type, id } = fromGlobalId(globalId);
     const repository = context.connection.getRepository(type);
-    return repository.findOneById(id);
-}, (value: object) => {
-    return value.constructor.name;
-});
+    return repository.findOneById(id) as Promise<T>;
+}
+
+export const safeIdFetcher = async <T>(globalId: string, context: InterviewrResolverContext): Promise<T> => {
+    const instance = await idFetcher<T>(globalId, context);
+
+    if (!instance) {
+        throw 'Unknown globalId';
+    }
+
+    return instance;
+}
+
+export const { nodeInterface, nodeField } = nodeDefinitions<InterviewrResolverContext>(
+    idFetcher, 
+    (value: object) => {
+        return value.constructor.name;
+    }
+);
