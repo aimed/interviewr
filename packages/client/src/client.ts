5048e336ccad2bf69ea11c5d65c45672aba42657
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-02-16 10:58:26 +0100

    fix(apollo): allow cookies for auth

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -7,6 +7,8 @@ export const client = new ApolloClient({
   //  `/graphql` endpoint on the same host
   // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
   // to a different host
-  link: new HttpLink(),
+  link: new HttpLink({
+    credentials: 'include'
+  }),
   cache: new InMemoryCache(),
 });
