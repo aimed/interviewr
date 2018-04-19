08ef3135bbb4af1e64958948e84559664ff7f8da
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-04-19 08:41:10 +0200

    feat: disable service worker

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -6,12 +6,10 @@ import * as ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { App } from './App';
 import { client } from './client';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-      <App />
+    <App />
   </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
