3b294a2bfd5a222adfe35fd3238822a83a875d1f
Maximilian Täschner <maximilian.taeschner@gmail.com>
2018-07-08 09:44:17 +0200

    feat: adds proficiency to skills

--- packages/server/src/entities/Skill.ts
+++ packages/server/src/entities/Skill.ts
@@ -2,9 +2,12 @@ import { Field, ID, ObjectType } from 'type-graphql';
 
 @ObjectType()
 export class Skill {
-    @Field(() => ID)
+    @Field(type => ID)
     public id: number;
 
     @Field()
     public description: string;
+
+    @Field({ nullable: true, description: 'A value between 1 and 5 indicating how well the skill is known' })
+    public proficiency: number;
 }
